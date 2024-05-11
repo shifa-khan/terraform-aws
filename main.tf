d251b9b0c56d76419fc1296cc88941b3864fc83b
Shifa Khan <67899867+shifa-khan@users.noreply.github.com>
2024-05-11 15:50:54 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,24 @@
-resource "aws_s3_bucket" "example" {
-  bucket = "name-of-the-bucket"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
+terraform {
+  required_version = ">= 0.12"
 }
+
+# CONFIGURE OUR AWS CONNECTION
+
+provider "aws" {}
+
+# CREATE THE S3 BUCKET
+
+data "aws_caller_identity" "current" {}
+
+locals {
+  account_id    = data.aws_caller_identity.current.account_id
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  # With account id, this S3 bucket names can be *globally* unique.
+  bucket = "${local.account_id}-terraform-states"
+
+  # Enable versioning so we can see the full revision history of our
+  versioning {
+    enabled = true
+  }
